2ab641a5bc5d4ac6e4ef44d581ada02cc0398621
Your Name <youremail@gmail.com>
2026-01-16 15:46:17 +0700

    Add admin lesson CRUD pages (FE)

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import LessonList from "./pages/admin/lessons/LessonList";
 import LessonCreate from "./pages/admin/lessons/LessonCreate";
 import LessonEdit from "./pages/admin/lessons/LessonEdit";
@@ -8,10 +8,16 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* ✅ vào / sẽ tự nhảy qua trang list */}
+        <Route path="/" element={<Navigate to="/admin/lessons" replace />} />
+
         <Route path="/admin/lessons" element={<LessonList />} />
         <Route path="/admin/lessons/create" element={<LessonCreate />} />
         <Route path="/admin/lessons/edit/:id" element={<LessonEdit />} />
         <Route path="/admin/lessons/:id" element={<LessonDetail />} />
+
+        {/* ✅ nếu gõ sai link */}
+        <Route path="*" element={<h2 style={{ padding: 20 }}>404 Not Found</h2>} />
       </Routes>
     </BrowserRouter>
   );
